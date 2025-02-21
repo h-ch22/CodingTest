db3071fe8cab23e5c1b05978bb3d869e6d64b91e
Changjin Ha <ckdwls9460@gmail.com>
2025-02-21 17:24:26 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 756 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,10 @@
+import sys
+
+a, b, c, d, e, f = map(int, sys.stdin.readline().rstrip().split(' '))
+x, y = 0, 0
+
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            print(x, y)
+            sys.exit(0)
