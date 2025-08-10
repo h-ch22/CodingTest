ed16d2b564533b8ac43968e242130aaa10b5522b
Changjin Ha <ckdwls9460@gmail.com>
2025-08-10 17:15:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 296 ms, Memory: 35052 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,58 @@
+import sys
+from collections import deque
+
+dfs_visited = []
+def dfs(v: int, graph: dict[int, list[int]]):
+    dfs_visited.append(v)
+
+    if v in graph.keys():
+        for nxt in graph[v]:
+            if nxt not in dfs_visited:
+                dfs(nxt, graph)
+
+n, m, v = map(int, sys.stdin.readline().rstrip().split())
+graph: dict[int, list[int]] = {}
+
+for _ in range(m):
+    l, r = map(int, sys.stdin.readline().rstrip().split())
+
+    if l in graph.keys():
+        graph[l].append(r)
+
+    else:
+        graph[l] = [r]
+
+    if r in graph.keys():
+        graph[r].append(l)
+    else:
+        graph[r] = [l]
+
+for k in graph.keys():
+    graph[k].sort()
+
+dfs(v, graph)
+
+bfs_visited = []
+q = deque([v])
+bfs_visited.append(v)
+
+while q:
+    v = q.popleft()
+
+    if v in graph.keys():
+        for nxt in graph[v]:
+            if nxt not in bfs_visited:
+                bfs_visited.append(nxt)
+                q.append(nxt)
+
+dfs_output = ''
+bfs_output = ''
+
+for d in dfs_visited:
+    dfs_output += f'{str(d)} '
+
+for b in bfs_visited:
+    bfs_output += f'{str(b)} '
+
+print(dfs_output.rstrip())
+print(bfs_output.rstrip())
