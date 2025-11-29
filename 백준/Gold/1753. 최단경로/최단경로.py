d0cd687b4238f0a1d85825ef1a4b1f8d54c11950
Changjin Ha <ckdwls9460@gmail.com>
2025-11-29 23:22:16 +0900

    [Gold IV] Title: 최단경로, Time: 560 ms, Memory: 71428 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,8 +1,8 @@
-import heapq
-import sys
 import math
+import sys
+import heapq
 
-def dijkstra(start, graph, distances):
+def dijkstra(graph, start, distances):
     queue = []
     heapq.heappush(queue, (0, start))
     distances[start] = 0
@@ -13,27 +13,30 @@ def dijkstra(start, graph, distances):
         if distances[n] < d:
             continue
 
-        for i in graph[n]:
-            c = d + i[1]
+        for node in graph[n]:
+            weight, next = node[0], node[1]
 
-            if c < distances[i[0]]:
-                distances[i[0]] = c
-                heapq.heappush(queue, (c, i[0]))
+            cost = d + weight
 
-n, e = map(int, sys.stdin.readline().strip().split())
-distances = [math.inf] * (n + 1)
-graph = [[] for _ in range(n + 1)]
-start = int(sys.stdin.readline().strip())
+            if cost < distances[next]:
+                distances[next] = cost
+                heapq.heappush(queue, (cost, next))
+
+
+v, e = map(int, sys.stdin.readline().strip().split())
+k = int(sys.stdin.readline().strip())
+distances = [math.inf] * (v + 1)
+graph = [[] for _ in range(v + 1)]
 
 for _ in range(e):
-    u, v, w = map(int, sys.stdin.readline().strip().split())
-    graph[u].append((v, w))
+    start, end, weight = map(int, sys.stdin.readline().strip().split())
+    graph[start].append((weight, end))
 
-dijkstra(start, graph, distances)
+dijkstra(graph, k, distances)
 
-for i in range(1, n + 1):
-    if distances[i] == math.inf:
+for d in range(1, v + 1):
+    if distances[d] == math.inf:
         print('INF')
 
     else:
-        print(distances[i])
+        print(distances[d])
