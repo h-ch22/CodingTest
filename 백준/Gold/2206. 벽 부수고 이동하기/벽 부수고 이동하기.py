1bb68c74d84e9b87111178142ada01062df70cc8
Changjin Ha <ckdwls9460@gmail.com>
2025-12-06 17:14:51 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3408 ms, Memory: 189712 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+def bfs():
+    # 0: 벽을 부수지 않음, 1: 벽을 부숨
+    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+    queue = deque([(0, 0, 0)])
+    visited[0][0][0] = 1
+
+    while queue:
+        x, y, broken = queue.popleft()
+
+        if x == m - 1 and y == n - 1:
+            return visited[y][x][broken]
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+
+            if 0 <= nx < m and 0 <= ny < n:
+                if graph[ny][nx] == 0 and visited[ny][nx][broken] == 0:
+                    visited[ny][nx][broken] = visited[y][x][broken] + 1
+                    queue.append((nx, ny, broken))
+
+                elif graph[ny][nx] == 1 and broken == 0:
+                    if visited[ny][nx][1] == 0:
+                        visited[ny][nx][1] = visited[y][x][broken] + 1
+                        queue.append((nx, ny, 1))
+
+    return -1
+
+n, m = map(int, sys.stdin.readline().strip().split())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().strip())))
+
+print(bfs())
