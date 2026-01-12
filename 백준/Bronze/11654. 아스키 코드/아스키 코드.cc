88c0eb93ea1150e4967be19b21bee5c4473db494
Changjin Ha <ckdwls9460@gmail.com>
2026-01-12 21:59:14 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    char c;
+    cin >> c;
+    cout << static_cast<int>(c);
+
+    return 0;
+}
