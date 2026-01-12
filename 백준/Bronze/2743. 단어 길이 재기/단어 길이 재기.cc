e955473b806ab8d9bce208297995c96be0e426df
Changjin Ha <ckdwls9460@gmail.com>
2026-01-12 21:51:48 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string str;
+    cin >> str;
+
+    cout << str.length();
+
+    return 0;
+}
