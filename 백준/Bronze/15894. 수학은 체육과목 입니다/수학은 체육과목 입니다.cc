5eda55f688578138d9be8fa2703bab1bbadfeb8b
Changjin Ha <ckdwls9460@gmail.com>
2026-01-20 20:28:03 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    long n;
+    cin >> n;
+
+    long result = n * 4;
+    cout << result << "\n";
+    
+    return 0;
+}
