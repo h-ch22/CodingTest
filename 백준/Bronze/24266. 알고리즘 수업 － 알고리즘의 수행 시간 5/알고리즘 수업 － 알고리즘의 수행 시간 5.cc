0be870301318bd7975b211eff616f04fe62860c7
Changjin Ha <ckdwls9460@gmail.com>
2026-01-30 21:22:45 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    long long n;
+    cin >> n;
+
+    cout << n * n * n << "\n" << 3;
+    return 0;
+}
