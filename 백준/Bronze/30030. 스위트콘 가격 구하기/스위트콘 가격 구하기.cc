d65ebe0b6e85447169d196bf7b08200ced411acd
Changjin Ha <ckdwls9460@gmail.com>
2026-02-13 22:39:49 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int b;
+    cin >> b;
+
+    cout << b / 1.1 << "\n";
+
+    return 0;
+}
