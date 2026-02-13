cfdf3ef58bc2a33ed45733523f5a5803f46051b3
Changjin Ha <ckdwls9460@gmail.com>
2026-02-13 22:36:38 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    long long a, b;
+    cin >> a >> b;
+
+    cout << a * b << "\n";
+
+    return 0;
+}
