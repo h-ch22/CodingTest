b88051fc0d59b493cd685c893af53e6e79997e7c
Changjin Ha <ckdwls9460@gmail.com>
2026-02-20 12:43:33 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1);
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = ((dp[i - 1] % 10007) + ((2 * dp[i - 2]) % 10007)) % 10007;
+    }
+
+    cout << dp[n] << "\n";
+
+    return 0;
+}
