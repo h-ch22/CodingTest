f4fc367ef0ae8ede08cf424acb1740016e0f82fe
Changjin Ha <ckdwls9460@gmail.com>
2026-03-01 18:15:47 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -7,34 +7,38 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
+    
     int n;
     cin >> n;
-
-    vector<vector<int>> costs(n, vector<int>(3));
-    vector<vector<int>> dp(n, vector<int>(3));
-
-    for (int i = 0; i < n; i++) {
-        cin >> costs[i][0] >> costs[i][1] >> costs[i][2];
-    }
-
-    dp[0][0] = costs[0][0];
-    dp[0][1] = costs[0][1];
-    dp[0][2] = costs[0][2];
-
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (j == 0) {
-                dp[i][j] = costs[i][j] + min(dp[i - 1][1], dp[i - 1][2]);
-            } else if (j == 1) {
-                dp[i][j] = costs[i][j] + min(dp[i - 1][0], dp[i - 1][2]);
-            } else {
-                dp[i][j] = costs[i][j] + min(dp[i - 1][0], dp[i - 1][1]);
-            }
+    
+    vector<vector<int>> matrix(n, vector<int>(3));
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> matrix[i][j];
         }
     }
-
-    cout << *min_element(dp[n - 1].begin(), dp[n - 1].end()) << "\n";
-
+    
+    vector<vector<int>> dp(n, vector<int>(3));
+    dp[0] = matrix[0];
+    
+    for(int i = 1; i < n; i++) {
+        dp[i][0] = matrix[i][0] + min(
+            dp[i - 1][1], dp[i - 1][2]
+        );
+        
+        dp[i][1] = matrix[i][1] + min(
+            dp[i - 1][0], dp[i - 1][2]
+        );
+        
+        dp[i][2] = matrix[i][2] + min(
+            dp[i - 1][0], dp[i - 1][1]    
+        );
+    }
+    
+    int answer = *min_element(dp[n - 1].begin(), dp[n - 1].end());
+    
+    cout << answer << "\n";
+    
     return 0;
 }
