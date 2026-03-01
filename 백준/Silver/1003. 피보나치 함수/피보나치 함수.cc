07b0fa71a7c3d51f4376815962ab35abc48a38ae
Changjin Ha <ckdwls9460@gmail.com>
2026-03-01 15:14:23 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,13 +1,3 @@
-/*
- * fib(0) -> [1, 0]
- * fib(1) -> [0, 1]
- * fib(2) -> [1, 1]
- * fib(3) -> [1, 2]
- * fib(4) -> [2, 3]
- * fib(5) -> {3, 5]
- * fib(n) -> [fib(n-2).first + fib(n-1).first, fib(n-2).second + fib(n-1).second]
- */
-
 #include <iostream>
 #include <vector>
 
@@ -16,24 +6,29 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
+    
+    vector<pair<int, int>> dp(41);
+    dp[0] = make_pair(1, 0);
+    dp[1] = make_pair(0, 1);
+    dp[2] = make_pair(1, 1);
+    
     int T;
     cin >> T;
-
-    while (T--) {
+    
+    while(T--) {
         int n;
         cin >> n;
-        vector<pair<int, int>> dp(n + 1);
-
-        dp[0] = make_pair(1, 0);
-        dp[1] = make_pair(0, 1);
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = make_pair(dp[i - 2].first + dp[i - 1].first, dp[i - 2].second + dp[i - 1].second);
+        
+        for(int i = 3; i <= n; i++) {
+            int cntOfZero = dp[i - 1].first + dp[i - 2].first;
+            
+            int cntOfOne = dp[i - 1].second + dp[i - 2].second;
+            
+            dp[i] = make_pair(cntOfZero, cntOfOne);
         }
-
+        
         cout << dp[n].first << " " << dp[n].second << "\n";
     }
-
+    
     return 0;
 }
