127222871f144d28dccbedef12e52de941ec117f
Changjin Ha <ckdwls9460@gmail.com>
2026-03-01 14:39:19 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,40 +1,35 @@
-/*
- * 1 -> (0)
- * 2 -> (1)
- * 3 -> (1)
- * 4 -> 2 -> 1 (2)
- * 5 -> 4 -> 2 -> 1 (3)
- */
-
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+#define MAX 1000001
 
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
+    
     int n;
     cin >> n;
-
-    vector<int> memo(n + 1, 0);
-    memo[2] = 1;
-    memo[3] = 1;
-
-    for (int i = 4; i <= n; i++) {
-        if (i % 2 != 0 && i % 3 != 0) {
-            memo[i] = memo[i - 1] + 1;
-        } else if (i % 2 == 0 && i % 3 == 0) {
-            memo[i] = min(memo[i - 1] + 1, min(memo[i / 2] + 1, memo[i / 3] + 1));
-        } else if (i % 2 == 0) {
-            memo[i] = min(memo[i - 1] + 1, memo[i / 2] + 1);
-        } else if (i % 3 == 0) {
-            memo[i] = min(memo[i - 1] + 1, memo[i / 3] + 1);
+    
+    vector<int> dp(MAX);
+    dp[2] = 1;
+    dp[3] = 1;
+    
+    for(int i = 4; i <= n; i++) {
+        dp[i] = dp[i - 1] + 1;
+        
+        if(i % 2 == 0) {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+        
+        if(i % 3 == 0) {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
         }
     }
-
-    cout << memo[n] << "\n";
-
+    
+    cout << dp[n] << "\n";
+    
     return 0;
 }
