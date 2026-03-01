4582394779c38794d4cbb584078cf95325162cdb
Changjin Ha <ckdwls9460@gmail.com>
2026-03-01 22:43:22 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3076 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -7,34 +7,37 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
+    
     int n;
     cin >> n;
-
-    vector<vector<long long>> t(n);
-
-    for (int i = 0; i < n; i++) {
-        t[i] = vector<long long>(i + 1);
-
-        for (int j = 0; j < i + 1; j++) {
-            cin >> t[i][j];
+    
+    vector<vector<int>> triangle(n);
+    
+    for(int i = 1; i <= n; i++) {
+        vector<int> row(i);
+        
+        for(int j = 0; j < i; j++) {
+            cin >> row[j];
         }
+        
+        triangle[i - 1] = row;
     }
-
-    vector<vector<long long>> dp(n);
-    dp[n - 1] = t[n - 1];
-
-    for (int i = n - 2; i >= 0; i--) {
-        vector<long long> tmp(i + 1);
-
-        for (int j = 0; j <= i; j++) {
-            tmp[j] = (t[i][j] + max(dp[i + 1][j], dp[i + 1][j + 1]));
+    
+    vector<vector<int>> dp(n);
+    
+    dp[0] = triangle[n - 1];
+    
+    for(int i = 1; i < n; i++) {
+        vector<int> row(n - i);
+        
+        for(int j = 0; j < n - i; j++) {
+            row[j] = triangle[n - i - 1][j] + max(dp[i - 1][j], dp[i - 1][j + 1]);
         }
-
-        dp[i] = tmp;
+        
+        dp[i] = row;
     }
-
-    cout << dp[0][0] << "\n";
-
+    
+    cout << dp[n - 1][0] << "\n";
+    
     return 0;
 }
