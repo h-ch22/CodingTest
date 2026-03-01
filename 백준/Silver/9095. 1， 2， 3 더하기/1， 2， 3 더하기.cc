9878355a6b28656123be59a9bc84262ec453cde4
Changjin Ha <ckdwls9460@gmail.com>
2026-03-01 14:58:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,42 +1,3 @@
-/*
- * - 1 (1)
- * 1
- *
- * - 2 (2)
- * 1 + 1
- * 2
- *
- * - 3 (4)
- * 1 + 1 + 1
- * 2 + 1
- * 1 + 2
- * 3
- *
- * - 4 (7)
- * 1 + 1 + 1 + 1
- * 1 + 1 + 2
- * 1 + 2 + 1
- * 2 + 1 + 1
- * 2 + 2
- * 1 + 3
- * 3 + 1
- *
- * - 5 (13)
- * 1 + 1 + 1 + 1 + 1
- * 1 + 1 + 1 + 2
- * 1 + 1 + 2 + 1
- * 1 + 2 + 1 + 1
- * 2 + 1 + 1 + 1
- * 1 + 2 + 2
- * 2 + 1 + 2
- * 2 + 2 + 1
- * 1 + 1 + 3
- * 1 + 3 + 1
- * 3 + 1 + 1
- * 3 + 2
- * 2 + 3
- */
-
 #include <iostream>
 #include <vector>
 
@@ -45,30 +6,25 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
+    
     int T;
     cin >> T;
-
-    while (T--) {
+    
+    while(T--) {
         int n;
         cin >> n;
-
-        if (n < 2) {
-            cout << 1 << "\n";
-            continue;
-        }
-
-        vector<int> dp(n + 1);
+        
+        vector<int> dp(12);
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-
-        for (int i = 4; i <= n; i++) {
+        
+        for(int i = 4; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-
+        
         cout << dp[n] << "\n";
     }
-
+    
     return 0;
 }
