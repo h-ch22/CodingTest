40bd7ca28624a6999fa461d833c46708eb2f698b
Changjin Ha <ckdwls9460@gmail.com>
2026-03-03 22:30:18 +0900

    [Bronze V] Title: Hack-a-Holics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34443. Hack－a－Holics/Hack－a－Holics.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int c, n, p;
+    cin >> c >> n >> p;
+    
+    cout << c * p << "\n";
+    
+    return 0;
+}
