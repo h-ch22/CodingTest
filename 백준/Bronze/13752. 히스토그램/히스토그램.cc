1e6ed33a9426885b6cc1a0697451dd312e69492a
Changjin Ha <ckdwls9460@gmail.com>
2026-03-10 22:22:57 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        
+        for(int i = 0; i < num; i++) {
+            cout << '=';
+        }
+        
+        cout << "\n";
+    }
+    
+    return 0;
+}
