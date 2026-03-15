b763f6096a81ac5409611a86ae7275958226cf85
Changjin Ha <ckdwls9460@gmail.com>
2026-03-15 23:19:36 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    long long n, m;
+    cin >> n >> m;
+    
+    cout << (n == m ? 1 : 0) << "\n";
+    
+    return 0;
+}
