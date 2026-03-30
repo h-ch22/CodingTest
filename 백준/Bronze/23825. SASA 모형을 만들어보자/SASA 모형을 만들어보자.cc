3b13c9565dd4f057877dc27bbfb463d295a0d361
Changjin Ha <ckdwls9460@gmail.com>
2026-03-30 22:40:10 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    cout << min(n, m) / 2 << "\n";
+
+    return 0;
+}
