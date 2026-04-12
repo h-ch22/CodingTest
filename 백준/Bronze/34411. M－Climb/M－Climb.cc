0e8277ba7f648cd466d44b4ad1d09fbb5a3f15f9
Changjin Ha <ckdwls9460@gmail.com>
2026-04-12 22:34:32 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m, n, y;
+    cin >> m >> n >> y;
+
+    cout << m * n << "\n";
+
+    return 0;
+}
