255591f78ad51ef3603b2da25a0ac359cf530059
Changjin Ha <ckdwls9460@gmail.com>
2026-04-18 14:49:00 +0900

    [level 2] Title: 주차 요금 계산, Time: 4.28 ms, Memory: 3.98 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -20,44 +20,36 @@ vector<string> parseRecord(const string& record) {
     return words;
 }
 
-double parseTime(const string& start, const string& end) {
+int parseTime(const string& start, const string& end) {
     stringstream ss_start(start);
     stringstream ss_end(end);
     
     string segment;
-    vector<double> startTime;
-    vector<double> endTime;
-        
+    vector<int> startTime;
+    vector<int> endTime;
+    
     while(getline(ss_start, segment, ':')) {
-        startTime.emplace_back(stod(segment));
+        startTime.emplace_back(stoi(segment));
     }
     
     while(getline(ss_end, segment, ':')) {
-        endTime.emplace_back(stod(segment));
+        endTime.emplace_back(stoi(segment));
     }
         
     if(startTime[0] == endTime[0]) {
         return endTime[1] - startTime[1];
-    } else if(endTime[1] == startTime[1]) {
-        return (endTime[0] - startTime[0]) * 60;
     } else if(endTime[0] - startTime[0] == 1 && endTime[1] == 0) {
         return 60 - startTime[1];
     } else {
-        double diffHours = (endTime[0] - startTime[0]) * 60;
-        double diffMinutes;
+        int diffHours = (endTime[0] - startTime[0]) * 60;
+        int diffMinutes;
         
         if(endTime[1] == 0) {
             diffMinutes = 60 - startTime[1];
-            diffHours -= 60;
         } else {
-            if(endTime[1] < startTime[1]) {
-                endTime[1] += 60;
-                diffHours -= 60;
-            }
-            
-            diffMinutes = endTime[1] - startTime[1];
+            diffMinutes = max(endTime[1] - startTime[1], startTime[1] - endTime[1]);
         }
-                
+        
         return diffHours + diffMinutes;
     }
 }
@@ -69,30 +61,24 @@ vector<int> solution(vector<int> fees, vector<string> records) {
     for(const string& r: records) {
         vector<string> log = parseRecord(r);
         
-        if(log[2] == "IN" && logs[log[1]].size() % 2 != 0) {
-            logs[log[1]].emplace_back("23:59");
-        }
-        
         logs[log[1]].emplace_back(log[0]);
     }
     
     for(const auto& pair: logs) {
-        double eta = 0;
-        
-        if(pair.second.size() % 2 != 0) {
+        if(pair.second.size() == 1) {
             logs[pair.first].emplace_back("23:59");
         }
         
-        for(int i = 0; i < pair.second.size(); i+=2) {
-            eta += parseTime(pair.second[i], pair.second[i+1]);
-        }
-        
+        int eta = parseTime(pair.second[0], pair.second[1]);
+        cout << pair.first << " -> " << pair.second[0] << " - " << pair.second[1] << " = " << eta << " -> ";
         if(eta <= fees[0]) {
             answer.emplace_back(fees[1]);
+            cout << fees[1] << "\n";
         } else {
             int fee = fees[1];
-            double extraFee = ceil((eta - fees[0]) / fees[2]) * fees[3];
-            answer.emplace_back(fee + static_cast<int>(extraFee));
+            double extraFee = ceil((eta - fees[2]) / 10) * fees[3];
+            answer.emplace_back(fee + extraFee);
+            cout << fee + extraFee << "\n";
         }
     }
     
