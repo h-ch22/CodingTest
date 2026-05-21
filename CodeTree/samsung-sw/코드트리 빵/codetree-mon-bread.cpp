ea815042a6ffad4b2b158e62ce027837cf703cf2
Changjin Ha <ckdwls9460@gmail.com>
2026-05-21 09:16:07 +0900

    [Passed] 코드트리 빵 (2ms, 0MB)

--- CodeTree/samsung-sw/코드트리 빵/codetree-mon-bread.cpp
+++ CodeTree/samsung-sw/코드트리 빵/codetree-mon-bread.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <deque>
 #include <algorithm>
-#define MAX_DIST 30
+#include <deque>
 
 using namespace std;
 
-int dx[4] = { -1, 0, 0, 1 };
-int dy[4] = { 0, -1, 1, 0 };
-
-int n, m;
-vector<vector<int>> grid; // 0: 빈 칸, 1: 베이스캠프, 2: 벽 (진입불가)
+int n, m, currentTurn;
+vector<vector<int>> grid; // 0: 빈 공간, 1: 베이스캠프, 2: 지나갈 수 없는 구역
 vector<pair<int, int>> stores;
+vector<pair<int, int>> camps;
 vector<pair<int, int>> people;
-vector<pair<int, int>> camp;
 
-vector<vector<int>> getStoreDistance(const int x, const int y) {
+int dx[4] = { -1, 0, 0, 1 }; // 세로
+int dy[4] = { 0, -1, 1, 0 }; // 가로
+
+/*
+* getDistance(int x, int y): vector<vector<int>>
+* x: 가고자 하는 곳의 x
+* y: 가고자 하는 곳의 y
+* 해당 좌표를 기준으로 모든 좌표에 대해 거리를 반환한다.
+*/
+vector<vector<int>> getDistance(const int x, const int y) {
     deque<pair<int, int>> queue;
-    vector<vector<int>> distance(n, vector<int>(n, -1));
-
-    distance[x][y] = 0;
     queue.emplace_back(x, y);
 
+    vector<vector<int>> distance(n, vector<int>(n, -1));
+    distance[x][y] = 0;
+
     while(!queue.empty()) {
-        const pair<int, int> current = queue.front();
+        pair<int, int> current = queue.front();
         queue.pop_front();
 
         for(int i = 0; i < 4; i++) {
             int nx = current.first + dx[i];
             int ny = current.second + dy[i];
+            
+            if(nx == x && ny == y) {
+                continue;
+            }
 
             if(0 <= nx && nx < n && 0 <= ny && ny < n) {
-                if(grid[nx][ny] < 2 && distance[nx][ny] == -1) {
+                if(grid[nx][ny] != 2 && distance[nx][ny] == -1) {
                     distance[nx][ny] = distance[current.first][current.second] + 1;
                     queue.emplace_back(nx, ny);
                 }
@@ -42,89 +51,94 @@ vector<vector<int>> getStoreDistance(const int x, const int y) {
     return distance;
 }
 
-bool isStore(const int x, const int y) {
-    for(const pair<int, int>& s: stores) {
-        if(s.first == x && s.second == y) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-void movePerson(const int peopleIdx, const vector<vector<int>>& distance) {
-    int minStep = MAX_DIST;
-    pair<int, int> toMove;
-
+/*
+* getShortestPlace(vector<vector<int>> distance, int x, int y): pair<int, int>
+* vector<vector<int>> distance: 가고자 하는 곳을 기준으로 모든 맵의 거리
+* int x: 현재 x
+* int y: 현재 y
+* 현재 x, y를 기준으로 가장 가까운 좌표를 반환한다.
+*/
+pair<int, int> getShortestPlace(const vector<vector<int>>& distance, const int x, const int y) {
+    int shortest = 15 * 15;
+    pair<int, int> result = { -1, -1 };
+    
     for(int i = 0; i < 4; i++) {
-        int nx = people[peopleIdx].first + dx[i];
-        int ny = people[peopleIdx].second + dy[i];
+        int nx = x + dx[i];
+        int ny = y + dy[i];
 
         if(0 <= nx && nx < n && 0 <= ny && ny < n) {
-            if(distance[nx][ny] < minStep && distance[nx][ny] != -1) {
-                minStep = distance[nx][ny];
-                toMove.first = nx;
-                toMove.second = ny;
+            if(distance[nx][ny] < shortest && distance[nx][ny] != -1) {
+                shortest = distance[nx][ny];
+                result = { nx, ny };
             }
         }
     }
 
-    people[peopleIdx] = toMove;
+    return result;
 }
 
-void moveToShortestCamp(const int peopleIdx, const vector<vector<int>>& distance) {
-    int minStep = MAX_DIST;
-    vector<pair<int, int>> mins;
-
-    for(int i = 0; i < camp.size(); i++) {
-        int cx = camp[i].first, cy = camp[i].second;
-        int dist = distance[cx][cy];
-
-        if(dist < minStep && dist != -1 && grid[cx][cy] != 2) {
-            minStep = dist;
-            mins.clear();
-            mins.emplace_back(cx, cy);
-        } else if(dist == minStep) {
-            mins.emplace_back(cx, cy);
-        }
-    }
-
-    sort(mins.begin(), mins.end(), [](const pair<int, int>& a, const pair<int, int>& b){
-        if(a.first == b.first) {
-            return a.second < b.second;
-        }
-
-        return a.first < b.first;
-    });
-
-    people[peopleIdx].first = mins[0].first;
-    people[peopleIdx].second = mins[0].second;
-
-    grid[mins[0].first][mins[0].second] = 2;
+/*
+* block(int x, int y): void
+* x: 막고자 하는 구역의 x
+* y: 막고자 하는 구역의 y
+* x, y를 지나갈 수 없도록 막는다.
+*/
+void block(const int x, const int y){
+    grid[x][y] = 2;
 }
 
-void block() {
+/*
+* getAllPeopleInStore(): void
+* 모든 사람이 스토어에 진입했는지 확인한 결과를 반환한다.
+*/
+
+bool getAllPeopleInStore() {
     for(int i = 0; i < m; i++) {
-        int personX = people[i].first, personY = people[i].second;
-        int storeX = stores[i].first, storeY = stores[i].second;
-
-        if(personX == storeX && personY == storeY) {
-            grid[personX][personY] = 2;
-        }
-    }
-}
-
-bool isAllPeopleInStore() {
-    for(int i = 0; i < m; i++) {
-        int personX = people[i].first, personY = people[i].second;
-        int storeX = stores[i].first, storeY = stores[i].second;
-
-        if(personX != storeX || personY != storeY) {
+        if(people[i] != stores[i]) {
             return false;
         }
     }
 
     return true;
+}
+
+/*
+* getShortestCamp(vector<vector<int>>& distance, int x, int y): pair<int, int>
+* vector<vector<int>> distance: 가고자 하는 곳을 기준으로 맵 전역의 거리
+* 가고자 하는 곳을 기준으로 가장 가까운 캠프의 위치를 반환한다.
+*/
+pair<int, int> getShortestCamp(const vector<vector<int>>& distance) {
+    int shortest = 15 * 15;
+    vector<pair<int, int>> shortestPaths;
+
+    for(const pair<int, int> c: camps) {
+        const int distX = c.first;
+        const int distY = c.second;
+
+        if(grid[distX][distY] < 2) {
+            if(distance[distX][distY] < shortest && distance[distX][distY] != -1) {
+                shortest = distance[distX][distY];
+                shortestPaths.clear();
+                shortestPaths.emplace_back(distX, distY);
+            } else if(distance[distX][distY] == shortest) {
+                shortestPaths.emplace_back(distX, distY);
+            }
+        }
+    }
+
+    if(shortestPaths.size() == 1) {
+        return shortestPaths[0];
+    } else {
+        sort(shortestPaths.begin(), shortestPaths.end(), [](const pair<int, int> a, const pair<int, int> b) {
+            if(a.first == b.first) {
+                return a.second < b.second;
+            }
+
+            return a.first < b.first;
+        });
+
+        return shortestPaths[0];
+    }
 }
 
 int main() {
@@ -136,63 +150,73 @@ int main() {
     stores.resize(m);
     people.resize(m);
 
+    currentTurn = 1;
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            int f;
-            cin >> f;
+            int tmp;
+            cin >> tmp;
 
-            if(f == 1) {
-                camp.emplace_back(i, j);
+            if(tmp == 1) {
+                camps.emplace_back(i, j);
             }
 
-            grid[i][j] = f;
+            grid[i][j] = tmp;
         }
     }
 
     for(int i = 0; i < m; i++) {
         int x, y;
         cin >> x >> y;
-
-        stores[i].first = x - 1;
-        stores[i].second = y - 1;
+        stores[i] = { x - 1, y - 1 };
+        people[i] = { -1, -1 };
     }
-
-    for(int i = 0; i < m; i++) {
-        people[i].first = -1;
-        people[i].second = -1;
-    }
-
-    int t = 0;
-
-    // for(const pair<int, int>& s: stores) {
-    //     cout << s.first << " " << s.second << "\n";
-    // }
 
     while(true) {
-        // 모든 사람들이 편의점에 진입하면 종료
-        if(isAllPeopleInStore()) {
-            cout << t << "\n";
+        // 1. 모든 사람이 편의점에 진입했는지 확인하고, 진입했으면 멈춘다.
+        if(getAllPeopleInStore()) {
+            cout << currentTurn - 1 << "\n";
             break;
         }
 
-        // 1 ~ m번 사람까지 편의점을 향해 한 칸 전진
-        for(int i = 0; i < (t < m ? t : m); i++) {
-            if(people[i].first > -1 && people[i].second > -1 && people[i] != stores[i]) {
-                vector<vector<int>> distance = getStoreDistance(stores[i].first, stores[i].second);
-                movePerson(i, distance);
+        // 2-1. 격자에 있는 사람들 중 편의점에 있지 않는 모두를 편의점 방향으로 1칸 움직인다.
+        int range = (currentTurn < m) ? currentTurn : m;
+        vector<pair<int, int>> toBlock(range);
+
+        for(int i = 0; i < range; i++) {
+            if(stores[i] != people[i]) {
+                vector<vector<int>> distance = getDistance(stores[i].first, stores[i].second);
+                int humanX = people[i].first, humanY = people[i].second;
+                
+                if(humanX < 0 || humanY < 0) {
+                    continue;
+                }
+    
+                pair<int, int> shortest = getShortestPlace(distance, humanX, humanY);
+                toBlock[i] = shortest;
+                people[i] = shortest;
             }
         }
 
-        // 사람들이 모두 움직인 후 해당 위치가 편의점이라면 벽을 세움
-        block();
-
-        // t번 사람을 가장 가까이 있는 베이스 캠프로 넣기
-        if(t < m) {
-            vector<vector<int>> distance = getStoreDistance(stores[t].first, stores[t].second);
-            moveToShortestCamp(t, distance);
+        // 2-2. 격자에 있는 사람들이 모두 움직이고 나면 해당 좌표를 막는다.
+        for(int i = 0; i < range; i++) {
+            if(stores[i] == toBlock[i]) {
+                block(toBlock[i].first, toBlock[i].second);
+            }
         }
 
-        t++;
+        // 3. 현재 시간이 t <= m일 때 t번 사람이 편의점에 있지 않는 경우 t번 사람을 가까이 있는 베이스 캠프로 진입시킨다.
+        if(currentTurn <= m) {
+            if(stores[currentTurn - 1] != people[currentTurn - 1]) {
+                vector<vector<int>> distance = getDistance(stores[currentTurn - 1].first, stores[currentTurn - 1].second);
+                pair<int, int> toGo = getShortestCamp(distance);
+                people[currentTurn - 1] = toGo;
+                block(toGo.first, toGo.second);
+            }
+        }
+
+        // 4. 현재 시간 증가
+        currentTurn++;
     }
 
     return 0;
