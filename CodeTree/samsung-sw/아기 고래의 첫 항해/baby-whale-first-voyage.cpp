b40fae0ba778cb2eef5dd6a181a3a0b0436b96fc
Changjin Ha <ckdwls9460@gmail.com>
2026-06-28 14:00:10 +0900

    [Passed] 아기 고래의 첫 항해 (9ms, 1MB)

--- CodeTree/samsung-sw/아기 고래의 첫 항해/baby-whale-first-voyage.cpp
+++ CodeTree/samsung-sw/아기 고래의 첫 항해/baby-whale-first-voyage.cpp
@@ -5,12 +5,9 @@
 
 using namespace std;
 
-struct GridMap;
-/*
-d: 1 - 상, 2 - 하, 3 - 좌, 4 - 우
-*/
+struct Cell;
 int n, r, c, d, globalOrder;
-vector<vector<GridMap>> grid;
+vector<vector<Cell>> grid;
 
 const int DIRECTIONS[5][4] = {
     { 0, 0, 0, 0 },
@@ -23,124 +20,141 @@ const int DIRECTIONS[5][4] = {
 const int dr[4] = { 0, 1, 0, -1 };
 const int dc[4] = { -1, 0, 1, 0 };
 
-struct GridMap {
-    int r, c, type, order;
+struct Cell {
+    int r, c, order;
+    bool isReef;
 
-    GridMap() {
-        order = -1;
+    Cell() {
+        order = 1e6;
+        isReef = true;
     }
 
-    GridMap(const int r, const int c, const int type) {
+    Cell(const int r, const int c, const bool isReef) {
         this->r = r;
         this->c = c;
-        this->type = type;
-        order = type == 0 ? 0 : -1;
+        this->isReef = isReef;
+        this->order = isReef ? 1e6 : 0;
     }
 
     void updateOrder() {
+        if(isReef) return;
+
         order = ++globalOrder;
     }
 
-    bool operator <(const GridMap& other) const {
+    bool operator <(const Cell& other) const {
         return order < other.order;
     }
 };
 
-pair<int, int> getDirection(const int dir) {
-    switch(dir) {
+pair<int, int> getNextDirections(const int d) {
+    switch(d) {
         case 1:
-            return make_pair(-1, 0);
+            return { -1, 0 };
 
         case 2:
-            return make_pair(1, 0);
+            return { 1, 0 };
 
         case 3:
-            return make_pair(0, -1);
+            return { 0, -1 };
 
         case 4:
-            return make_pair(0, 1);
+            return { 0, 1 };
 
         default:
-            return make_pair(0, 0);
+            return { 0, 0 };
     }
 }
 
-int getDirection(const pair<int, int> dir) {
-    if(dir.first == -1) {
+int getNewDirection(const pair<int, int> d) {
+    if(d.first == -1) {
         return 1;
     }
-    
-    if(dir.first == 1) {
+
+    if(d.first == 1) {
         return 2;
     }
 
-    if(dir.second == -1) {
+    if(d.second == -1) {
         return 3;
     }
 
-    if(dir.second == 1) {
+    if(d.second == 1) {
         return 4;
     }
 
     return 0;
 }
 
-bool goToAdjacentArea() {
+bool experienceToAdjacent() {
+    int currentD = d;
+
     for(int i = 0; i < 4; i++) {
-        const int currentD = DIRECTIONS[d][i];
-        const pair<int, int> direction = getDirection(currentD);
-        const int nr = r + direction.first, nc = c + direction.second;
+        const int newD = DIRECTIONS[d][i];
+        const pair<int, int> nextDir = getNextDirections(newD);
+        const int nr = nextDir.first + r, nc = nextDir.second + c;
 
         if(0 <= nr && nr < n && 0 <= nc && nc < n) {
-            if(grid[nr][nc].order == 0) {            
-                // cout << r << ", " << c << " -> " << nr << ", " << nc << " (" << d << " -> " << currentD << ")\n";
-                r = nr;
-                c = nc;
-                d = currentD;
+            if(grid[nr][nc].isReef || grid[nr][nc].order > 0) continue;
 
-                grid[nr][nc].updateOrder();
-                
-                return true;
-            }
+            // cout << "From " << r << ", " << c << " to " << nr << ", " << nc << "\n";
+
+            d = newD;
+            r = nr;
+            c = nc;
+            grid[nr][nc].updateOrder();
+
+            return true;
         }
     }
 
     return false;
 }
 
-pair<int, int> getNearest() {
-    vector<vector<int>> dist(n, vector<int>(n, -1));
-    vector<pair<int, int>> candidates;
+pair<int, int> getShortestPlace() {
     deque<pair<int, int>> queue;
+    vector<vector<int>> dist(n, vector<int>(n, -1));
 
-    queue.emplace_back(r, c);
     dist[r][c] = 0;
+    queue.emplace_back(r, c);
 
-    int shortest = 1e6;
+    int minDist = 1e6;
+    vector<pair<int, int>> candidates;
+
+    bool shouldBreak = false;
 
     while(!queue.empty()) {
+        if(shouldBreak) break;
+        
         const pair<int, int> current = queue.front();
         queue.pop_front();
 
-        for(int i = 0; i < 4; i++) {
-            const int nr = current.first + dr[i], nc = current.second + dc[i];
+        const int cr = current.first, cc = current.second;
 
-            if(0 <= nr && nr < n && 0 <= nc && nc < n && dist[nr][nc] == -1) {
-                if(grid[nr][nc].type == 0) {
-                    dist[nr][nc] = dist[current.first][current.second] + 1;
+        for(int i = 0; i < 4; i++) {
+            const int nr = cr + dr[i], nc = cc + dc[i];
+
+            if(0 <= nr && nr < n && 0 <= nc && nc < n) {
+                if(grid[nr][nc].isReef) {
+                    dist[nr][nc] = 1e6;
+                    continue;
+                }
+
+                if(dist[nr][nc] == -1) {
+                    dist[nr][nc] = dist[cr][cc] + 1;
                     queue.emplace_back(nr, nc);
 
-                    if(grid[nr][nc].order == 0) {
-                        if(dist[nr][nc] < shortest) {
-                            shortest = dist[nr][nc];
+                    if(grid[nr][nc].order == 0) {      
+                        if(dist[nr][nc] > minDist) shouldBreak = true;
+
+                        if(dist[nr][nc] < minDist) {
                             candidates.clear();
+                            minDist = dist[nr][nc];
                             candidates.emplace_back(nr, nc);
-                        } else if(dist[nr][nc] == shortest) {
+                        } else if(dist[nr][nc] == minDist) {
                             candidates.emplace_back(nr, nc);
                         }
                     }
-                } else {
-                    dist[nr][nc] = 1e6;
                 }
             }
         }
@@ -148,49 +162,56 @@ pair<int, int> getNearest() {
 
     sort(candidates.begin(), candidates.end(), [](const pair<int, int>& a, const pair<int, int>& b){
         if(a.first == b.first) return a.second < b.second;
-
+        
         return a.first < b.first;
-    });
+    }); 
 
-    // cout << "From: " << r << ", " << c << " To: " << candidates[0].first << ", " << candidates[0].second << "\n";
+    for(const pair<int, int>& c: candidates) {
+        if(c.first == r && c.second == ::c) continue;
 
-    return candidates[0];
+        return c;
+    }
+
+    return { 0, 0 };
 }
 
-void move(const pair<int, int> to) {
+void moveToPlace(const pair<int, int> target) {
     deque<pair<int, int>> queue;
-    queue.emplace_back(to.first, to.second);
-
     vector<vector<int>> dist(n, vector<int>(n, -1));
-    dist[to.first][to.second] = 0;
+
+    const int tr = target.first, tc = target.second;
+    dist[tr][tc] = 0;
+
+    queue.emplace_back(tr, tc);
 
     while(!queue.empty()) {
         const pair<int, int> current = queue.front();
+        const int cr = current.first, cc = current.second;
+
         queue.pop_front();
 
         for(int i = 0; i < 4; i++) {
-            const int nr = current.first + dr[i], nc = current.second + dc[i];
+            const int nr = cr + dr[i], nc = cc + dc[i];
 
             if(0 <= nr && nr < n && 0 <= nc && nc < n) {
-                if(grid[nr][nc].type == 0) {
-                    if(dist[nr][nc] == -1) {
-                        dist[nr][nc] = dist[current.first][current.second] + 1;
-                        queue.emplace_back(nr, nc);
-                    }
-                } else {
+                if(grid[nr][nc].isReef) {
                     dist[nr][nc] = 1e6;
+                    continue;
+                }
+
+                if(dist[nr][nc] == -1) {
+                    dist[nr][nc] = dist[cr][cc] + 1;
+                    queue.emplace_back(nr, nc);
                 }
             }
         }
     }
 
-    int currentR = r, currentC = c;
+    int currentR = r, currentC = c, currentD = d;
 
     while(true) {
-        if(currentR == to.first && currentC == to.second) {
-            r = currentR;
-            c = currentC;
-
+        if(currentR == tr && currentC == tc) {
+            r = tr, c = tc, d = currentD;
             break;
         }
 
@@ -198,12 +219,15 @@ void move(const pair<int, int> to) {
             const int nr = currentR + dr[i], nc = currentC + dc[i];
 
             if(0 <= nr && nr < n && 0 <= nc && nc < n) {
+                if(grid[nr][nc].isReef) continue;
+
                 if(dist[nr][nc] == dist[currentR][currentC] - 1) {
-                    currentR = nr;
-                    currentC = nc;
-                    d = getDirection(make_pair(dr[i], dc[i]));
-                    
-                    if(grid[nr][nc].order == 0) grid[nr][nc].updateOrder();
+                    currentR = nr, currentC = nc, currentD = getNewDirection({ dr[i], dc[i] });
+
+                    if(grid[nr][nc].order == 0) {
+                        grid[nr][nc].updateOrder();
+                    }
+
                     break;
                 }
             }
@@ -211,7 +235,7 @@ void move(const pair<int, int> to) {
     }
 }
 
-bool checkAllSeaVisited() {
+bool checkAllCellExperienced() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(grid[i][j].order == 0) return false;
@@ -227,46 +251,46 @@ int main() {
 
     globalOrder = 0;
     cin >> n >> r >> c >> d;
-    grid.assign(n, vector<GridMap>(n));
 
-    int current = 0;
+    grid.assign(n, vector<Cell>(n));
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             int type;
             cin >> type;
 
-            grid[i][j] = GridMap(i, j, type);
+            grid[i][j] = Cell(i, j, type == 1);
         }
     }
 
     --r;
     --c;
+
     grid[r][c].updateOrder();
 
     while(true) {
-        if(checkAllSeaVisited()) break;
+        if(checkAllCellExperienced()) break;
 
-        if(!goToAdjacentArea()) {
-            pair<int, int> to = getNearest();
-            move(to);
+        if(!experienceToAdjacent()) {
+            const pair<int, int> target = getShortestPlace();
+            // cout << "From " << r << ", " << c << " To " << target.first << ", " << target.second << "\n";
+
+            moveToPlace(target);
         }
-
-        current++;
     }
 
-    vector<GridMap> flatten;
+    vector<Cell> flatten;
 
-    for(const auto& row: grid) {
-        flatten.insert(flatten.end(), row.begin(), row.end());
+    for(int row = 0; row < n; row++) {
+        flatten.insert(flatten.end(), grid[row].begin(), grid[row].end());
     }
 
     sort(flatten.begin(), flatten.end());
 
-    for(const GridMap& f: flatten) {
-        if(f.type == 0) {
-            cout << f.r + 1 << " " << f.c + 1 << "\n";
-        }
+    for(const Cell& f: flatten) {
+        if(f.isReef) continue;
+
+        cout << f.r + 1 << " " << f.c + 1 << "\n";
     }
 
     return 0;
